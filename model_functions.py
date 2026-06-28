7ac6b7edd33e9b272cd7a24acc36192fa9fa252e
Prathamesh Bhurke <prathameshbhurke666@gmail.com>
2026-06-28 17:17:52 +0530

    Successful Day 2 of completion of the playlist

--- model_functions.py
+++ model_functions.py
@@ -362,7 +362,7 @@ def create_playlist_from_dataframe(unique_songs, start_date, end_date, collabora
         if "id" not in playlist:
             st.error(f"Failed to create playlist: {playlist}")
             return
-        playlist_id = "4ar8iua1eZDmraYonUQDC0" #playlist["id"]
+        playlist_id = playlist["id"]
 
         # Add tracks
         progress_text.text("⏳ Step 3/3: Adding tracks...")
@@ -387,7 +387,7 @@ def create_playlist_from_dataframe(unique_songs, start_date, end_date, collabora
         import json
 
         track_uris = []
-        subset = unique_songs.iloc[666:]   # first 690 songs
+        subset = unique_songs.iloc[:600]   # first 600 songs
         
         for i, (_, row) in enumerate(subset.iterrows(), start=667):
             track_id = search_spotify_track(
