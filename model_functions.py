fe7b0ab2e6c6dccd5b7d2b3d26122ed3d47faa59
Prathamesh Bhurke <prathameshbhurke666@gmail.com>
2026-07-26 13:22:35 +0530

    Day 1 Atlantic Playlist Creation Successfull!

--- model_functions.py
+++ model_functions.py
@@ -419,9 +419,9 @@ def create_playlist_from_dataframe(unique_songs, start_date, end_date, collabora
         import json
 
         track_uris = []
-        subset = unique_songs.iloc[:666]   # first 600 songs
+        subset = unique_songs.iloc[666:]   # first 600 songs
         
-        for i, (_, row) in enumerate(subset.iterrows()):
+        for i, (_, row) in enumerate(subset.iterrows(), start=666):
             track_id = search_spotify_track(
                 row["song"], row["artist"],
                 {"Authorization": f"Bearer {access_token}"}
